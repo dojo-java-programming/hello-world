9fd305b5e2af1457a1569cf354d3ebf367690bba
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-04-12 16:32:31 +0200

    #1: Adds test code

--- src/test/java/com/github/javadojoprogramming/helloworld/AppTest.java
+++ src/test/java/com/github/javadojoprogramming/helloworld/AppTest.java
@@ -1,10 +1,18 @@
 package com.github.javadojoprogramming.helloworld;
 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
 public class AppTest {
 
 	@Test
 	public void getMessage() throws Exception {
+		App app = new App();
 		
+		assertEquals("Hello John!", app.getMessage("John"));
 	}
 
 }

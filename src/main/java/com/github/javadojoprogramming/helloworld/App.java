014265a23f8da8a58fb71921a2fee57d53ccd4a2
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-04-19 12:25:40 +0200

    3: Check GetMessage parameter name
    Task-Url:
    https://github.com/dojo-java-programming/hello-world/issues/issue/3

--- src/main/java/com/github/javadojoprogramming/helloworld/App.java
+++ src/main/java/com/github/javadojoprogramming/helloworld/App.java
@@ -3,6 +3,9 @@ package com.github.javadojoprogramming.helloworld;
 public class App {
 
 	public String getMessage(final String name) {
+		if (name == null) {
+			return "Hello World!";
+		}
 		return "Hello " + name + "!";
 	}
 
--- src/test/java/com/github/javadojoprogramming/helloworld/AppTest.java
+++ src/test/java/com/github/javadojoprogramming/helloworld/AppTest.java
@@ -11,8 +11,15 @@ public class AppTest {
 	@Test
 	public void getMessage() throws Exception {
 		App app = new App();
-		
+
 		assertEquals("Hello John!", app.getMessage("John"));
+	}
+
+	@Test
+	public void getMessageNull() throws Exception {
+		App app = new App();
+
+		assertEquals("Hello World!", app.getMessage(null));
 	}
 
 }
